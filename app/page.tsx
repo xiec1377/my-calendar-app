b9492047c3386d486f36628d5d45bdf35ea68a81
Catherine Xie <catherinexie2014@gmail.com>
2025-11-01 18:16:35 -0400

    build: install react-big-calendar library for monthly, weekly, daily views

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import { Calendar } from "@/components/ui/calendar";
+import { Calendar as SmallCalendar} from "@/components/ui/calendar";
 import {
   Dialog,
   DialogClose,
@@ -20,10 +20,16 @@ import {
   PopoverContent,
   PopoverTrigger,
 } from "@/components/ui/popover";
+import { format } from "date-fns";
 // radix
+
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import moment from "moment";
+import "react-big-calendar/lib/css/react-big-calendar.css";
 
 export default function Home() {
   const [date, setDate] = useState<Date | undefined>(new Date());
+  const localizer = momentLocalizer(moment);
   return (
     // max-w-3xl
     <div className="flex w-full items-start justify-center bg-zinc-50 font-sans dark:bg-black">
@@ -54,49 +60,45 @@ export default function Home() {
               <div className="grid gap-4">
                 <Label htmlFor="date">Time</Label>
                 <div className="flex flex-row gap-2">
-                <Popover>
-                  <PopoverTrigger asChild>
-                    <Button
-                      variant="outline"
-                      className="flex-[2] justify-start text-left"
-                    >
-                      {/* <CalendarIcon className="mr-2 h-4 w-4" /> */}
-                      {date ? "10-10-10" : <span>Pick a date</span>}
-                    </Button>
-                  </PopoverTrigger>
-                  <PopoverContent className="w-auto p-0" align="start">
-                    <Calendar
-                      mode="single"
-                      selected={date}
-                      onSelect={setDate}
-                      initialFocus
-                    />
-                  </PopoverContent>
-                </Popover>
-                <Input
-                  id="date"
-                  name="date"
-                  defaultValue="@peduarte"
-                  type="time"
-                  className="flex-[1]"
-                ></Input>
-                -
-                <Input
-                  id="date"
-                  name="date"
-                  defaultValue="@peduarte"
-                  type="time"
-                  className="flex-[1]"
-                ></Input>
+                  <Popover>
+                    <PopoverTrigger asChild>
+                      <Button
+                        variant="outline"
+                        className="flex-[2] justify-start text-left"
+                      >
+                        {/* <CalendarIcon className="mr-2 h-4 w-4" /> */}
+                        {date ? format(date, "PPP") : "Pick a date"}
+                      </Button>
+                    </PopoverTrigger>
+                    <PopoverContent className="w-auto p-0" align="start">
+                      <Calendar
+                        mode="single"
+                        selected={date}
+                        onSelect={setDate}
+                        initialFocus
+                      />
+                    </PopoverContent>
+                  </Popover>
+                  <Input
+                    id="date"
+                    name="date"
+                    defaultValue="@peduarte"
+                    type="time"
+                    className="flex-[1]"
+                  ></Input>
+                  -
+                  <Input
+                    id="date"
+                    name="date"
+                    defaultValue="@peduarte"
+                    type="time"
+                    className="flex-[1]"
+                  ></Input>
                 </div>
               </div>
               <div className="grid gap-4">
                 <Label htmlFor="notes">Notes</Label>
-                <Input
-                  id="notes"
-                  name="name"
-                  placeholder="Add notes here..."
-                />
+                <Input id="notes" name="name" placeholder="Add notes here..." />
               </div>
               <DialogFooter>
                 <DialogClose asChild>
@@ -107,21 +109,35 @@ export default function Home() {
             </DialogContent>
           </form>
         </Dialog>
-        <Calendar
+        <SmallCalendar
           mode="single"
           selected={date}
           onSelect={setDate}
           className="rounded-md border shadow-sm"
           captionLayout="dropdown"
         />
+        {/* <div style={{ height: "600px" }}>
+          <Calendar
+            localizer={localizer}
+            // events={events}
+            startAccessor="start"
+            endAccessor="end"
+            views={"month"}
+            defaultView="month"
+          />
+        </div> */}
       </div>
       <main className="flex w-full flex-row items-center justify-between bg-white dark:bg-black sm:items-start">
-        <Calendar
-          mode="single"
-          selected={date}
-          onSelect={setDate}
-          className="rounded-lg border w-full h-full"
-        />
+        <div style={{ height: "600px" }}>
+          <Calendar
+            localizer={localizer}
+            // events={events}
+            startAccessor="start"
+            endAccessor="end"
+            views={["month", "week", "day"]}
+            defaultView="month"
+          />
+        </div>
       </main>
     </div>
   );
